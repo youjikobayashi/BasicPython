e94f418fea52bdd1c965a40c291e0cdc3d99c428
youjikobayashi <y.kobayashi.24.3.1287@4nonome.com>
2024-03-19 20:51:42 +0900

    修正２

--- prime_number.py
+++ prime_number.py
@@ -1,14 +1,13 @@
 
 
 # TODO
-n = 61
+n = 3
+if n == 1:
+    print("素数ではない。")
+if n == 2:
+    print ("素数である。")
 for a in range(2,n):
-    if n == 1:
-        print ("素数ではない。")
-        break
-    if n == 2:
-        print ("素数である。")
-        break
+    
         
     if n%a == 0:
         print ("素数ではない。")
@@ -18,13 +17,11 @@ for a in range(2,n):
             print ("素数である。")
 
 n = 10
+if n == 1:
+    print("素数ではない。")
+if n == 2:
+    print ("素数である。")
 for a in range(2,n):
-    if n == 1:
-        print ("素数ではない。")
-        break
-    if n == 2:
-        print ("素数である。")
-        break
     if n%a == 0:
         print ("素数ではない。")
         break
