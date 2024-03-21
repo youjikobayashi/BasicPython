a69c5e3862d012e9d4ba796b3220ceeb2b61c61c
youjikobayashi <y.kobayashi.24.3.1287@4nonome.com>
2024-03-21 23:23:30 +0900

    修正４

--- euclid.py
+++ euclid.py
@@ -1,106 +1,17 @@
-a = 10
-b = 20
+a = int(input("a ="))
+b = int(input("b ="))
 
-# TODO
 if a>b:
-    for i in range(b):
-        c = a % b
-        if c == 0:
+    while b>0:
+        if a%b == 0:
             print(b)
             break
-        if b % c == 0:
-            print(c)
-            break
-        a = b % c
-        if a == 0:
-            print(b/c)
-            break
-        if c % a ==0:
-            print(a)
-            break
+        c = a%b
+        a, b = b, c
 else:
-
-    for i in range(a):
-        c = b % a
-        if c == 0:
+    while a>0:
+        if b%a ==0:
             print(a)
             break
-        if a % c == 0:
-            print(c)
-            break
-        b = a % c
-        if b == 0:
-            print(a/c)
-            break
-        if c % b == 0:
-            print(b)
-            break
-a = 14
-b = 91
-if a>b:
-    for i in range(b):
-        c = a % b
-        if c == 0:
-            print(b)
-            break
-        if b % c == 0:
-            print(c)
-            break
-        a = b % c
-        if a == 0:
-            print(b/c)
-            break
-        if c % a ==0:
-            print(a)
-            break
-else:
-    for i in range(a):
-        c = b % a
-        if c == 0:
-            print(a)
-            break
-        if a % c == 0:
-            print(c)
-            break
-        b = a % c
-        if b == 0:
-            print(a/c)
-            break
-        if c % b == 0:
-            print(b)
-            break
-a = 91
-b = 14
-if a>b:
-    for i in range(b):
-        c = a % b
-        if c == 0:
-            print(b)
-            break
-        if b % c == 0:
-            print(c)
-            break
-        a = b % c
-        if a == 0:
-            print(b/c)
-            break
-        if c % a ==0:
-            print(a)
-            break
-else:
-    for i in range(a):
-        c = b % a
-        if c == 0:
-            print(a)
-            break
-        if a % c == 0:
-            print(c)
-            break
-        b = a % c
-        if b == 0:
-            print(a/c)
-            break
-        if c % b == 0:
-            print(b)
-            break
-            
+        c = b%a
+        a,b = c , b
