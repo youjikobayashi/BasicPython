bdaba5a6fe38e2d15c0a39d2b8c46ab29755bc2e
youjikobayashi <y.kobayashi.24.3.1287@4nonome.com>
2024-03-24 15:03:56 +0900

    関数　課題

--- euclid.py
+++ euclid.py
@@ -1,17 +1,26 @@
-a = int(input("a ="))
-b = int(input("b ="))
+a = int(input())
+b = int(input())
+def euclid(a,b):
+    
+    if a>b:
+        while b>0:
+            if a%b == 0:
+                return b
+                break
+            c = a%b
+            a, b = b, c
+    else:
+        while a>0:
+            if b%a ==0:
+                return a
+                break
+            c = b%a
+            a,b = c , a
+        
+def so(a,b):
+    if euclid(a,b) == 1:
+        return True
+    else:
+        return False
 
-if a>b:
-    while b>0:
-        if a%b == 0:
-            print(b)
-            break
-        c = a%b
-        a, b = b, c
-else:
-    while a>0:
-        if b%a ==0:
-            print(a)
-            break
-        c = b%a
-        a,b = c , b
+print (so(a,b))
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,17 +1,24 @@
 from math import sin
 import math
-# --example--
-# print(sin(0))
-# >>> 0
-# -----------
-N = 100
-a = 0
-b = 0.5 *(math.pi)
-h =  (b - a) / N
-c = 1
-S = 0
-for c in range(N):
-    c +=1
-    S += h/2 * (sin(a+(c-1)*h) + sin(a+c*h))
-    
-print(S)
+
+def trape(f,N = 100,a = 0,b = 1):
+    h =  (b - a) / N
+    c = 1
+    S = 0
+    for c in range(N):
+        c +=1
+        S += h/2 * (f(a+(c-1)*h) + f(a+c*h))
+    return S
+
+def toiichi(x):
+    x = sin(x)
+    return x
+def toini(x):
+    x = 4/(1+x**2)
+    return x
+def toisan(x):
+    x = (math.pi **0.5) * (math.e**(-x**2))
+    return x
+print (trape(toiichi,50,0,math.pi * 0.5))
+print (trape(toini,100,0,1))
+print (trape(toisan,1000,-100,100))
